1bb7b23e029ecbae695f7c3b1f0a4994ae0abf03
bero285 <gioberia23@gmail.com>
2024-06-05 21:37:15 +0400

    Update styles for leftBar, navBar, and rightBar components

---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,41 @@
+import { Post } from "../post/Post";
+import "./posts.scss";
+
+export const Posts = () => {
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "lorem ipsum dolor sit amet consectetur.",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "lorem ipsum dolor sit amet consectetur.",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "lorem ipsum dolor sit amet consectetur.",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+  return (
+    <div className="posts">
+      {posts.map((post) => {
+        return <Post post={post} key={post.id} />;
+      })}
+    </div>
+  );
+};
